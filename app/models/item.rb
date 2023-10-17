abc4369f645d3302d2f2591b8bad68fb057a1371
NobuhideMine <141533543+NobuhideMine@users.noreply.github.com>
2023-10-17 10:56:12 +0000

    [Add] 管理者商品

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+    
+    has_many :cart_item, dependent: :destroy
+    has_many :order_detail, dependent: :destroy
+    belongs_to :genres
+    has_many :orders, through: :order_details
+
+    has_one_attached :image
+    
+
+    
 end
