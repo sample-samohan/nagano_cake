fae9d167407868135966f11856953d946a71df6d
mionaga <143914849+mionaga@users.noreply.github.com>
2023-10-18 03:53:20 +0000

    customer

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,17 +1,40 @@
 class Public::CustomersController < ApplicationController
+
   def show
+    @customer = current_customer
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      flash[:update]="お客様の情報更新に成功しました。"
+      redirect_to customers_mypage_path(@customer.id)
+    else
+     render  'edit'
+    end 
   end
-  
+
   def check
   end
-  
+
   def withdraw
-    
-  end  
+    @customer = Customer.find(current_customer.id)
+    # is_activeカラムをtrueに変更することにより削除フラグを立てる
+    @customer.update(is_active: true)
+    reset_session
+    flash[:notice] = "退会処理を実行いたしました"
+    redirect_to root_path
+
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:family_name, :first_name, :family_name_kana, :first_name_kana, :post_code, :address, :telephone_number)
+  end
+
 end
