fc4783d442b2124f695bbcc15651f004cc99b5b7
mionaga <143914849+mionaga@users.noreply.github.com>
2023-10-20 09:54:14 +0000

    address-finish

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,16 +1,49 @@
 class Public::AddressesController < ApplicationController
   def index
-  end
-
-  def edit
+    @address = Address.new
+    @addresses = Address.all
   end
 
   def create
+    address = Address.new(addresses_params)
+    address.customer_id = current_customer.id
+    if address.save
+      flash[:success] = "配送先を登録しました"
+      redirect_to addresses_path
+    else
+      flash[:danger] = "必要情報を入力してください"
+      redirect_to addresses_path
+    end  
+  end
+
+  def edit
+    @address = Address.find(params[:id])
   end
 
   def update
+    address = Address.find(params[:id])
+    if address.update(addresses_params)
+      flash[:success] = "編集を保存しました"
+      redirect_to addresses_path
+    else 
+      @address = Address.find(params[:id])
+      flash[:danger] = "必要情報を入力してください"
+      render "customers/addresses/edit"
+    end  
   end
 
   def destroy
+    address = Address.find(params[:id])
+    address.destroy
+    flash[:success] = "削除しました"
+    redirect_to addresses_path
   end
+  
+  private
+  
+  def addresses_params
+    params.require(:address).permit(:post_code, :address, :name)
+  end
+  
+  
 end
