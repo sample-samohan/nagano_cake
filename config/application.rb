2355538a3509843618b80a0176f87137f45dba9c
yamagisitutomu <ec2-user@ip-172-31-0-91.ap-northeast-1.compute.internal>
2023-10-23 11:01:47 +0000

    日時表示時間修正

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,8 @@ Bundler.require(*Rails.groups)
 module NaganoCake
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.time_zone = 'Tokyo'
+    config.i18n.default_locale = :ja
     config.load_defaults 6.1
 
     config.i18n.default_locale = :ja
