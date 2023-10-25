9772f389a9b0c1fba45410588aff988753e2ae48
yamagisitutomu <ec2-user@ip-172-31-0-91.ap-northeast-1.compute.internal>
2023-10-25 04:17:53 +0000

    コンフリクト修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,6 +8,17 @@ class Item < ApplicationRecord
 
     has_one_attached :image
     
+    def get_image(width, height)
+      unless image.attached?
+        file_path = Rails.root.join("app/assets/images/no_image.jpg")
+        image.attach(io: File.open(file_path), filename: "default-image.jpg", content_type: "image/jpeg")
+      end
+      image.variant(resize_to_fill: [width, height], gravity: :center).processed
+    end
+
+
+
+
     validates :name, presence: true
     validates :explanation, presence: true
     validates :non_taxed_price, presence: true, format: { with: /\A[0-9]+\z/i,} #半角数字のみ登録可能
